5f74cce817c4c678088e5089b696a0573d3c285a
Jason Baker <jason@onejasonforsale.com>
2020-03-23 21:53:50 -0700

    Add a bunch more stats.

--- history.js
+++ history.js
@@ -6,7 +6,7 @@ class CovidDataManager {
 		var self = this;
 		loadWebResource("https://covidtracking.com/api/us/daily", function(results) {
 			self.parseData(JSON.parse(results), "usData");
-			dataLoadedHandler();
+			dataLoadedHandler(self);
 		});
 	}
 
@@ -14,8 +14,13 @@ class CovidDataManager {
 		var recordArray = [];
 		var idCounter = 0;
 
-		for (var record of results) {
+		var lastRecordCopy = null;
+
+		console.log(results);
+		for (var i = results.length - 1; i >= 0; i--) {
+			
 			// original record from api: {"date":20200323,"states":56,"positive":42164,"negative":237321,"posNeg":279485,"pending":14571,"hospitalized":3325,"death":471,"total":294056}
+			var record = results[i];
 
 			var d = "" + record.date;
 			var year = parseInt(d.substr(0,4));
@@ -33,18 +38,53 @@ class CovidDataManager {
 			recordCopy.apiDate = record.date;
 			recordCopy.testResultsPositiveInt = record.positive;
 			recordCopy.testResultsNegativeInt = record.negative;
-			recordCopy.testResultsTotalInt = record.positive + record.negative;
+			recordCopy.testResultsTotalInt = record.positive + record.negative;			
 			recordCopy.hospitalizedInt = this.denull(record.hospitalized);
-			recordCopy.deathsInt = this.denull(record.death);
+			recordCopy.deathsInt = this.denull(record.death);			
+
+			//percentages on original api data
 			recordCopy.testResultsPositivePercentFloat = getPercentageFloat(recordCopy.testResultsPositiveInt, recordCopy.testResultsTotalInt);
 			recordCopy.testResultsNegativePercentFloat = getPercentageFloat(recordCopy.testResultsNegativeInt, recordCopy.testResultsTotalInt);
 			recordCopy.hospitalizedOfTotalPercentFloat = getPercentageFloat(recordCopy.hospitalizedInt, recordCopy.testResultsTotalInt);
 			recordCopy.hospitalizedOfPositivePercentFloat = getPercentageFloat(recordCopy.hospitalizedInt, recordCopy.testResultsPositiveInt);
 			recordCopy.deathsOfTotalPercentFloat = getPercentageFloat(recordCopy.deathsInt, recordCopy.testResultsTotalInt);
 			recordCopy.deathsOfPositivePercentFloat = getPercentageFloat(recordCopy.deathsInt, recordCopy.testResultsPositiveInt);
+
+			//new stats compared to previous day
+			var newStats = new Object();
+			newStats.testResultsPositiveInt = lastRecordCopy == null ? recordCopy.testResultsPositiveInt
+				: recordCopy.testResultsPositiveInt - lastRecordCopy.testResultsPositiveInt;
+
+			newStats.testResultsNegativeInt = lastRecordCopy == null ? recordCopy.testResultsNegativeInt
+				: recordCopy.testResultsNegativeInt - lastRecordCopy.testResultsNegativeInt;
+
+			newStats.testResultsTotalInt = lastRecordCopy == null ? recordCopy.testResultsTotalInt
+				: recordCopy.testResultsTotalInt - lastRecordCopy.testResultsTotalInt; 
+
+			newStats.hospitalizedInt = lastRecordCopy == null ? recordCopy.hospitalizedInt
+				: recordCopy.hospitalizedInt - lastRecordCopy.hospitalizedInt; 
+
+			newStats.deathsInt = lastRecordCopy == null ? recordCopy.deathsInt
+				: recordCopy.deathsInt - lastRecordCopy.deathsInt; 
+
+			newStats.testResultsPositivePercentFloat = getPercentageFloat(newStats.testResultsPositiveInt, newStats.testResultsTotalInt);
+			newStats.testResultsNegativePercentFloat = getPercentageFloat(newStats.testResultsNegativeInt, newStats.testResultsTotalInt);
+			newStats.hospitalizedOfTotalPercentFloat = getPercentageFloat(newStats.hospitalizedInt, newStats.testResultsTotalInt);
+			newStats.hospitalizedOfPositivePercentFloat = getPercentageFloat(newStats.hospitalizedInt, newStats.testResultsPositiveInt);
+			newStats.deathsOfTotalPercentFloat = getPercentageFloat(newStats.deathsInt, newStats.testResultsTotalInt);
+			newStats.deathsOfPositivePercentFloat = getPercentageFloat(newStats.deathsInt, newStats.testResultsPositiveInt);
+
+			newStats.testResultsPositiveGrowthPercentFloat = getPercentageFloat(newStats.testResultsPositiveInt, recordCopy.testResultsPositiveInt);
+			newStats.testResultsNegativeGrowthPercentFloat = getPercentageFloat(newStats.testResultsNegativeInt, recordCopy.testResultsNegativeInt);
+			newStats.testResultsTotalGrowthPercentFloat = getPercentageFloat(newStats.testResultsTotalInt, recordCopy.testResultsTotalInt);
+			newStats.hospitalizedGrowthPercentFloat = getPercentageFloat(newStats.hospitalizedInt, recordCopy.hospitalizedInt);
+			newStats.deathGrowthPercentFloat = getPercentageFloat(newStats.deathsInt, recordCopy.deathsInt);
+
+			recordCopy.newStats = newStats;
 			recordArray.push(recordCopy);
 
 			idCounter += 1;
+			lastRecordCopy = recordCopy;
 		}
 		this.unitedStatesDailyDataArray = recordArray;
 		console.log("data converted from api.", this.unitedStatesDailyDataArray);
